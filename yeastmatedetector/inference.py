8164b1537989b86cf2f93754f2eb5c0e211e7c21
David Bunk <bunk@biologie.uni-muenchen.de>
2021-09-10 14:51:17 +0200

    added unscale_results

--- yeastmatedetector/inference.py
+++ yeastmatedetector/inference.py
@@ -4,7 +4,7 @@ import torch
 import numpy as np
 from glob import glob
 from skimage.io import imread, imsave
-from skimage.transform import rescale
+from skimage.transform import rescale, resize
 from skimage.exposure import rescale_intensity
 
 from detectron2.config import get_cfg
@@ -22,10 +22,10 @@ class YeastMatePredictor():
         
         self.cfg = initialize_new_config_values(self.cfg)
 
+        self.cfg.merge_from_file(cfg)
+
         if not torch.cuda.is_available():
             self.cfg.MODEL.DEVICE = 'cpu'
-
-        self.cfg.merge_from_file(cfg)
 
         if weights is not None:
             self.cfg.MODEL.WEIGHTS = weights
@@ -50,7 +50,7 @@ class YeastMatePredictor():
         return image
 
     @staticmethod
-    def rescale_and_normalize(image, pixel_size, reference_pixel_size=110, lower_quantile=1.5, upper_quantile=98.5)
+    def rescale_and_normalize(image, pixel_size, reference_pixel_size=110, lower_quantile=1.5, upper_quantile=98.5):
         lq, uq = np.percentile(image, [lower_quantile, upper_quantile])
         image = rescale_intensity(image, in_range=(lq,uq), out_range=(0,1))
         image = image.astype(np.float32)
@@ -73,23 +73,43 @@ class YeastMatePredictor():
             possible_comps_dict[n+1] = new_comps
 
         things, mask = postproc_multimask(instances, possible_comps_dict, \
-            optional_object_score_threshold=optional_object_score_threshold, parent_override_thresh=parent_override_threshold, score_thresholds=score_thresholds)
+            optional_object_score_threshold=optional_object_score_threshold,\
+                 parent_override_thresh=parent_override_threshold, \
+                     score_thresholds=score_thresholds)
 
         return things, mask
 
-    def inference(self, image, pixel_size=110, reference_pixel_size=110, lower_quantile=1.5, upper_quantile=98.5):
+    @staticmethod
+    def unscale_results(things, mask, original_shape):
+        scale_factor = original_shape[0]/mask.shape[0]
 
+        if scale_factor != 1.0:
+            for key, thing in things.items():
+            things[key]['box'] = [int(x*scale_factor) for x in thing['box']]
+
+            mask = resize(mask, original_shape, preserve_range=True, anti_aliasing=False, order=0)
+
+        return things, mask
+
+
+    def inference(self, image, score_thresholds = {0:0.9, 1:0.5, 2:0.5}, pixel_size=110, reference_pixel_size=110, lower_quantile=1.5, upper_quantile=98.5):
+
+        original_shape = image.shape
         image = self.rescale_and_normalize(image, pixel_size, reference_pixel_size, lower_quantile, upper_quantile)
         image = self.image_to_tensor(image)
 
         with torch.no_grad():
-            instancse = self.model([image])[0]['instances']
+            instances = self.model([image])[0]['instances']
 
         possible_comps = self.cfg.POSTPROCESSING.POSSIBLE_COMPS
         optional_object_score_threshold = self.cfg.POSTPROCESSING.OPTIONAL_OBJECT_SCORE_THRESHOLD
         parent_override_threshold = self.cfg.POSTPROCESSING.PARENT_OVERRIDE_THRESHOLD
 
         things, mask = things, mask = postproc_multimask(instances, possible_comps, \
-            optional_object_score_threshold=optional_object_score_threshold, parent_override_thresh=parent_override_threshold)
+            optional_object_score_threshold=optional_object_score_threshold, \
+                parent_override_thresh=parent_override_threshold, \
+                    score_thresholds=score_thresholds)
 
+        things, mask = self.unscale_results(things, mask, original_shape)
+        
         return things, mask
